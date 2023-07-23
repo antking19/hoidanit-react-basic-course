4860661dee515803e61373951ed6dc2218cf723a
antking19 <cuong.truongkien@gmail.com>
2023-07-23 10:43:29 +0700

    Learn Component in ReactJS - video 16

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
+// import { useDispatch, useSelector } from "react-redux";
+// import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
+import MyComponent from "./components/MyComponent";
 const App = () => {
-    const count = useSelector((state) => state.counter.count);
-    const dispatch = useDispatch();
+    // const count = useSelector((state) => state.counter.count);
+    // const dispatch = useDispatch();
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello world hoi dan it</p>
-                <div>Count = {count}</div>
-                <button onClick={() => dispatch(increaseCounter())}>
-                    Increase
-                </button>
-                <button onClick={() => dispatch(decreaseCounter())}>
-                    Decrease
-                </button>
-            </header>
+        <div>
+            Hello world hoi dan it
+            <MyComponent />
         </div>
+        // <div className="App">
+        //     <header className="App-header">
+        //         <img src={logo} className="App-logo" alt="logo" />
+        //         <p>Hello world hoi dan it</p>
+        //         <div>Count = {count}</div>
+        //         <button onClick={() => dispatch(increaseCounter())}>
+        //             Increase
+        //         </button>
+        //         <button onClick={() => dispatch(decreaseCounter())}>
+        //             Decrease
+        //         </button>
+        //     </header>
+        // </div>
     );
 };
 
