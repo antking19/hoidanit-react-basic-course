40f1fb35cca3bf2261574fe2be2a83642387be12
antking19 <cuong.truongkien@gmail.com>
2023-07-23 23:55:26 +0700

    Outputting List - video 24

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,17 +3,23 @@ import UserInfor from "./UserInfor";
 import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
+    state = {
+        listUsers: [
+            { id: 1, name: "Kien Cuong", age: "33" },
+            { id: 2, name: "Marcus Cheung", age: "32" },
+            { id: 3, name: "Cuong Truong", age: "31" },
+        ],
+    };
+
     render() {
         const myInfor = ["ab", "c", "c"];
-
+        // DRY: Don't Repeat Yourself
         return (
             <div>
                 <UserInfor />
                 <br />
                 <br />
-                <DisplayInfor name="Cuong Truong" age="33" />
-                <hr />
-                <DisplayInfor name={"Kien Cuong"} age={26} myInfor={myInfor} />
+                <DisplayInfor listUsers={this.state.listUsers} />
             </div>
         );
     }
