e9b2f00ba261eaa6aacd3ff17314edfe8066f61d
antking19 <cuong.truongkien@gmail.com>
2023-07-23 21:27:04 +0700

    Props - video 23

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,19 @@
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
     render() {
+        const myInfor = ["ab", "c", "c"];
+
         return (
             <div>
                 <UserInfor />
+                <br />
+                <br />
+                <DisplayInfor name="Cuong Truong" age="33" />
+                <hr />
+                <DisplayInfor name={"Kien Cuong"} age={26} myInfor={myInfor} />
             </div>
         );
     }
