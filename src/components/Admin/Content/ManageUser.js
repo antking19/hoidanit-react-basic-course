064ea390052d8ae61f891320046296f5424bd03c
antking19 <cuong.truongkien@gmail.com>
2023-08-12 10:12:55 +0700

    api them moi nguoi dung - video 58

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,17 +1,29 @@
 import ModalCreateUser from "./ModalCreateUser";
+import "./ManageUser.scss";
+import { FcPlus } from "react-icons/fc";
+import { useState } from "react";
 
 const ManageUser = (props) => {
+    const [showModalCreateUser, setShowModalCreateUser] = useState(false);
+
     return (
         <div className="manage-user-container">
             <div className="title">Manage User</div>
-            <div className="user-content">
-                <div>
-                    <button>Add New Users</button>
+            <div className="users-content">
+                <div className="btn-add-new">
+                    <button
+                        className="btn btn-primary"
+                        onClick={() => setShowModalCreateUser(true)}
+                    >
+                        <FcPlus />
+                        Add New Users
+                    </button>
                 </div>
-                <div>
-                    Table User
-                    <ModalCreateUser />
-                </div>
+                <div className="table-users-container">Table User</div>
+                <ModalCreateUser
+                    show={showModalCreateUser}
+                    setShow={setShowModalCreateUser}
+                />
             </div>
         </div>
     );
